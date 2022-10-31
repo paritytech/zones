d3e7adb5e6e5aa8940298825e7c8735ca39bea8c
Harry Solovay <harrysolovay@gmail.com>
2022-10-31 10:19:27 -0400

    clean up rc and deduped example

--- examples/deduped.ts
+++ examples/deduped.ts
@@ -1,12 +1,22 @@
 import * as Z from "../mod.ts";
 
-let i = 0;
-
-function f() {
+let dupI = 0;
+function dup() {
   return Z.call(0, () => {
-    console.log(i++);
-    return "HELLO!";
+    return `${dupI++}`;
   });
 }
 
-await Z.runtime()(Z.ls(f(), f()));
+let dedupedI = 0;
+function deduped() {
+  return Z.call(0, function dedupedImpl() {
+    return `${dedupedI++}`;
+  });
+}
+
+const run = Z.runtime();
+
+const dupResult = run(Z.ls(dup(), dup())); // `["0", "1"]`
+const dedupedResult = run(Z.ls(deduped(), deduped())); // `["0", "0"]`
+
+console.log({ dupResult, dedupedResult });
