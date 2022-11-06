6a774cdb0833ac24becf4a59f133e536b2d2229a
Harry Solovay <harrysolovay@gmail.com>
2022-11-06 15:47:18 -0500

    delete nonsense example

--- examples/inspect.ts
+++
@@ -1,9 +0,0 @@
-import * as Z from "../mod.ts";
-
-const a = Z
-  .call(0, () => "HELLO")
-  .zoned("Greeting");
-const b = Z
-  .call(a, (aR) => [aR, "WORLD"])
-  .zoned("GreetingAndSubject");
-console.log(b);
